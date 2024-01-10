7dccd3d3e4f4a4e1e1dd46cb543589cc5a64c624
Sujal Shresth <sujshrest@example.com>
2024-01-10 16:14:08 +0530

    Assignment Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
   // code here
+  const data = imageData();
+  function createComp() {
+    return data.map((image) => {
+      return (
+        <img
+          key={image.id}
+          id={image.id}
+          src={image.img}
+          alt={`Image ${image.id}`}
+        />
+      );
+    });
+  }
+  return (
+    <div id="functionalComponent">
+      <h3>Kalvium Gallery using functionalComponent</h3>
+
+      <div className="grid">{createComp()}</div>
+    </div>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,52 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
+  };
+
+  createComp() {
+    const data = this.imageData();
+    return data.map((image) => {
+      return (
+        <img
+          key={image.id}
+          id={image.id}
+          src={image.img}
+          alt={`Image ${image.id}`}
+        />
+      );
+    });
   }
 
+  render() {
+    return (
+      <div id="classComponent">
+        <h3>Kalvium Gallery Using classComponent</h3>
+
+        <div className="grid">{this.createComp()}</div>
+      </div>
+    );
+  }
   // code here
 }
